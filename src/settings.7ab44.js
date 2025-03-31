9580c5dc95017ce52f39294c166f40489114b84a
vuong.vu <vuongcdt@gmail.com>
2025-03-31 23:01:14 +0700

    2

---
+++ src/settings.7ab44.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/main.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"d17e2",resources:"f09bf",main:"e87fa"}};
