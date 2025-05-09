bea3e17e8cc9e8f7a4c6a02c719b1be0afeb5bbf
vuong.vu <vuongcdt@gmail.com>
2025-05-09 09:01:57 +0700

    4

---
+++ src/settings.947dc.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/main.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"d0832",resources:"06879",main:"6ac78"}};
--- src/settings.fd9b1.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/main.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"52d36",resources:"06879",main:"bc37e"}};
